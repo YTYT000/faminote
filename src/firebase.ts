bc313de503e3421614f5ebc78d473c1a674dfee4
YTYT000 <no.na.000.lr@gmail.com>
2024-03-04 19:38:12 +0900

    修正

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDH7Wb_t9a-tWiD7otXgUmwrcjT-4esroE",
+  authDomain: "faminote-app.firebaseapp.com",
+  projectId: "faminote-app",
+  storageBucket: "faminote-app.appspot.com",
+  messagingSenderId: "627896385163",
+  appId: "1:627896385163:web:d2674cb1646132b03f5e8e",
+};
+
+initializeApp(firebaseConfig);
