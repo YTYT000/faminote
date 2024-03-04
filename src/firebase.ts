e3bca5de40cdff9cb5cd390971c483edc794d660
YTYT000 <no.na.000.lr@gmail.com>
2024-03-04 17:57:10 +0900

    Add Firebase workflow files

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxknz5vTKhzlm_miQ7eH6yE8IwJ875JtQ",
+  authDomain: "faminote-5c435.firebaseapp.com",
+  projectId: "faminote-5c435",
+  storageBucket: "faminote-5c435.appspot.com",
+  messagingSenderId: "437752505658",
+  appId: "1:437752505658:web:01d86b0cee5f01a1ff1c03",
+  measurementId: "G-F5WSL9F6YQ",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
